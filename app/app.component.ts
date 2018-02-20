02f480c38ed48baad06dbb291d45d81712f29f42
Sam Gespass <darth.toad@gmail.com>
2018-02-19 16:19:28 -0800

    add initial files

---
+++ app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <div class="container">
+    <h1>RECIPE BOX</h1>
+  </div>
+  `
+})
+
+export class AppComponent {
+
+}
---
+++ app/recipe.model.ts
@@ -0,0 +1,5 @@
+export class Recipe {
+  constructor(public name: string, public ingredients: string[], public instructions: string) {
+    
+  }
+}
